b406af2c5aab1fb3e5bb649f37521f4611d1fac1
Alek Pialucha <pialuchaa@btinternet.com>
2023-07-24 15:44:37 +0100

    flexible element function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement(
     "div",
     {},
     [
-      React.createElement("h1", {}, "Luna"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Havanese")
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed)
     ]
   )
 }
@@ -17,9 +17,21 @@ const App = () => {
     "div",
     {},
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Finn",
+      breed: "Short Hair Tabby"
+    }),
+    React.createElement(Pet, {
+      animal: "Fish",
+      name: "Nemo",
+      breed: "Clownfish"
+    })
   )
 };
 
