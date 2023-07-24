b4c2556268aff79b7a3f90a1ed2506da3bf49a4f
Alek Pialucha <pialuchaa@btinternet.com>
2023-07-24 18:36:30 +0100

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
-
-// const App = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       animal: "Dog",
-//       name: "Luna",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Cat",
-//       name: "Finn",
-//       breed: "Short Hair Tabby",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Fish",
-//       name: "Nemo",
-//       breed: "Clownfish",
-//     })
-//   );
-// };
-
+import ReactDOM from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
+  return(
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Finn" animal="Cat" breed="Short Hair Tabby" />
-    <Pet name="Nemo" animal="Fish" breed="Clownfish" />
+    <SearchParams/>
   </div>
+  )
 }
 
 const container = document.getElementById("root");
